fbceff00800eef3244d67fa033578095f2035e4c
BOND <bog.chumak@googlemail.com>
2025-04-15 11:34:06 +0200

    UPD BaseCharacterController ADD Cinemachine

--- Assets/Scripts/BaseCharacterController.cs
+++ Assets/Scripts/BaseCharacterController.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using static UnityEngine.InputSystem.InputAction;
 
+//BaseCharacter is the base of a character
 public class BaseCharacterController : MonoBehaviour
 {
+    private Vector2 movementInput;
+    [SerializeField] private float movementSpeed;
+
+    /// <summary>
+    /// Movement is called by the input system when the player moves the joystick or presses the arrow keys
+    /// </summary>
+    /// <param name="ctx">Context provided by Unity Input</param>
     public void Movement(CallbackContext ctx)
     {
-        Debug.Log($"Context: {ctx.ReadValue<Vector2>()}");
+        //movementInput is set by unity events
+        movementInput = ctx.ReadValue<Vector2>(); //comment
+        
+    }
+
+    //This is a update
+    private void Update()
+    {
+        transform.position += new Vector3(movementInput.x, movementInput.y, 0) * Time.deltaTime * movementSpeed;
     }
 }

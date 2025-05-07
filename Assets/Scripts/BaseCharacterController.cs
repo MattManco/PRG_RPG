da3b2ec6650f427a92733a0751b8dff20b163d2d
BOND <bog.chumak@googlemail.com>
2025-05-07 09:47:58 +0200

    UPD FightManager

--- Assets/Scripts/BaseCharacterController.cs
+++ Assets/Scripts/BaseCharacterController.cs
@@ -11,7 +11,7 @@ public class BaseCharacterController : MonoBehaviour
     [SerializeField] private float movementSpeed;
     [Range(0,1)][SerializeField] private float slowedFactor;
     private bool isSlowed;
-    private bool isPlayerInBattle;
+    private bool isPlayerPaused;
     private Vector3Int currentPosition;
     private Vector3Int lastEncounterPosition;
 
@@ -28,7 +28,7 @@ public class BaseCharacterController : MonoBehaviour
     private void Start()
     {
         isSlowed = false;
-        isPlayerInBattle = false;
+        isPlayerPaused = false;
     }
 
     /// <summary>
@@ -44,7 +44,7 @@ public class BaseCharacterController : MonoBehaviour
     //This is now a FIXEDupdate
     private void FixedUpdate()
     {
-        if (isPlayerInBattle) return;
+        if (isPlayerPaused) return;
         //var actuallMovementSpeed = isSlowed ? movementSpeed * slowedFactor : movementSpeed;
         var actualMovementSpeed = movementSpeed;
         if(isSlowed) actualMovementSpeed *= slowedFactor;
@@ -64,7 +64,7 @@ public class BaseCharacterController : MonoBehaviour
             if(currentPosition != lastEncounterPosition)
             {
                 lastEncounterPosition = currentPosition;
-                isPlayerInBattle = FightManager.Instance.CheckForEncounter();
+                isPlayerPaused = FightManager.Instance.CheckForEncounter();
             }
         }
     }
--- Assets/Scripts/FightManager.cs
+++ Assets/Scripts/FightManager.cs
@@ -1,9 +1,12 @@
+using System.Collections;
 using UnityEngine;
 
 public class FightManager : MonoBehaviour
 {
     public static FightManager Instance { get; private set; }
     [Range(0,100),SerializeField] private int chanceToEncounter;
+    [SerializeField] GameObject fightCanvas;
+    private bool isFightActive;
 
 
     // Start is called before the first frame update
@@ -17,15 +20,58 @@ public class FightManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        isFightActive = false;
     }
 
     public bool CheckForEncounter()
     {
         if (Random.Range(0, 100) < chanceToEncounter)
         {
-            Debug.Log("Start Encounter");
-            return true;
+            StartFight();
         }
-        return false;
+        return isFightActive;
+    }
+
+    private void StartFight()
+    {
+        fightCanvas.SetActive(true);
+        isFightActive = true;
+        StartCoroutine(FightCoroutine());
+    }
+
+    private IEnumerator FightCoroutine()
+    {
+        //Load Characters
+        LoadCharacter();
+        //Load Random Enemies
+        //Load BackgroundImages
+        //Load Music
+        //Load UI
+        //Load Items
+
+        /*while(transition){
+         * 
+         * DoStuff();
+         * yield return new WaitForEndOfFrame();
+         * 
+         * }*/
+
+        while (isFightActive)
+        {
+            //Check whos turn
+            //Make Player/ Enemies Turn
+            //Show and wait for end of Fight
+            //Set isFightActive to false <- GameOver? Enemies Death?
+            yield return new WaitForSeconds(3f);
+            isFightActive = false; //Because: Nobody got Time for this
+        }
+
+        //End Fight and gain XP and Gold
+        //Level UP?
+    }
+    private void LoadCharacter()
+    {
+
     }
 }
